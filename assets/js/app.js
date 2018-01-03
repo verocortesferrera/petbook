f146ccf82ee7472436cfa94a9a7e2efc6aea04a4
verocortesferrera <verocortesferrera@gmail.com>
2018-01-03 12:41:50 -0300

    arreglo conflictos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 	$('#myModal').modal('show');
 
+	$('#home').click(function(){
+console.log('hola');
+		$('#inicio').show();
+		$('#perfil').hide();
+		$('#message').hide();
+	})
 
+	$('#user').click(function(){
+		$('#inicio').hide();
+		$('#perfil').show();
+		$('#message').hide();
+	})
+
+	$('#mensaje').click(function(){
+		$('#inicio').hide();
+		$('#perfil').hide();
+		$('#message').show();
+	})
 });

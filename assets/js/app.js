899b94d089c42d621efeb7201804d405e6d40989
verocortesferrera <verocortesferrera@gmail.com>
2018-01-04 08:46:58 -0300

    posteo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 	$('#myModal').modal('show');
 
 	$('#boton2').click(function(){
-		console.log('hola');
 		$('#registro').hide();
 		$('#inicio').show();
 	})
 
+/*pestañas*/
 	$('#home').click(function(){
 console.log('hola');
 		$('#inicio').show();
@@ -25,4 +25,20 @@ console.log('hola');
 		$('#perfil').hide();
 		$('#message').show();
 	})
+
+	$(function(){
+		$('#post').on('click', function(){
+			console.log('hola');
+			var valo = $('#texto').val();
+				if(valo !==''){
+					var elem = $('<p></p>').text(valo);
+					$(elem).append('<button class ="rem btn-xs">x</button>');
+					$('#boxPost').append(elem);
+					$('#texto').val('');
+					$('.rem').on('click', function(){
+						$(this).parent().remove();
+					});
+				};
+		});
+	});
 });
